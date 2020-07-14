e13eb4c318fcba0e518e26e361d8d8cf6a18effc
Marcin Zogrodnik <marcin.zogrodnik@deeperthanblue.co.uk>
2020-07-14 08:58:43 +0100

    Remove webmentions 2

--- functions.php
+++ functions.php
@@ -119,7 +119,7 @@ function html5blank_header_scripts()
         wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
         wp_enqueue_script('modernizr'); // Enqueue it!
 
-        wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.5'); // Custom scripts
+        wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.6'); // Custom scripts
         wp_enqueue_script('html5blankscripts'); // Enqueue it!
     }
 }
@@ -498,8 +498,8 @@ function html5blankcomments($comment, $args, $depth)
 
     /**
      * Filter function used to remove the tinymce emoji plugin.
-     * 
-     * @param array $plugins 
+     *
+     * @param array $plugins
      * @return array Difference betwen the two arrays
      */
     function disable_emojis_tinymce($plugins)
